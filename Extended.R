6f8d3ca00bb098fa65f5d0c81bec17942e7d64cc
Aman Raj <amanraj@Amans-MacBook-Air.local>
2023-10-08 19:18:41 -0400

    Version8

--- Extended.R
+++ Extended.R
@@ -7,57 +7,65 @@ raters=unique(dices$rater_id)
 # ###################Turning Q_Overall rating to numeric from character################
 dices$Q_overall <- factor(dices$Q_overall, levels = c("No", "Unsure", "Yes"), ordered = TRUE)
 
-
+sumdices<-summary(dices)
 ###################Models###########################
 # Gender
-formula8<- Q_overall ~ rater_gender * (rater_raw_race + rater_age+rater_education+phase) + (1 | rater_id) + (1 | item_id)
+formula8<- Q_overall ~ rater_gender * (rater_raw_race + rater_age+rater_education) + (1 | rater_id) + (1 | item_id)
 
-formula9<-Q_overall ~ rater_gender * (rater_raw_race + rater_age + degree_of_harm+rater_education+phase) + (1 | rater_id) + (1 | item_id)
+formula9<-Q_overall ~ rater_gender * (rater_raw_race + rater_age + degree_of_harm+rater_education) + (1 | rater_id) + (1 | item_id)
 
-formula10 <- Q_overall ~ rater_gender *(rater_raw_race + rater_age + degree_of_harm+rater_education+phase) + (degree_of_harm | rater_id) + (1 | item_id)
+formula10 <- Q_overall ~ rater_gender *(rater_raw_race + rater_age + degree_of_harm+rater_education) + (degree_of_harm | rater_id) + (1 | item_id)
 
 priors8 <- get_prior(formula8, data = dices, family = cumulative("probit"))
 priors9 <- get_prior(formula9, data = dices, family = cumulative("probit"))
 priors10 <- get_prior(formula10, data = dices, family = cumulative("probit"))
 
-
-Model.intersectional.QS <- brm(
-  formula = formula2,
+prior8<-set_prior("normal()")
+Model.intersectional.AD.Gender <- brm(
+  formula = formula8,
   data = dices,
   family = cumulative("probit"),
-  prior = priors2,
+  prior = priors8,
   warmup = 1000,
-  iter = 4000,
+  iter = 2000,
   chains = 4,
   seed = 42,
   backend = 'rstan',
-  cores = 4
-)
-Model.intersectional.QS <- brm(
-  formula = formula2,
-  data = dices,
-  family = cumulative("probit"),
-  prior = priors2,
-  warmup = 1000,
-  iter = 4000,
-  chains = 4,
-  seed = 42,
-  backend = 'rstan',
-  cores = 4
-)
-Model.intersectional.QS <- brm(
-  formula = formula2,
-  data = dices,
-  family = cumulative("probit"),
-  prior = priors2,
-  warmup = 1000,
-  iter = 4000,
-  chains = 4,
-  seed = 42,
-  backend = 'rstan',
-  cores = 4
+  cores = 6
 )
 
+save(Model.intersectional.AD.Gender,file="ModelIntersectionalADGender.RData")
+
+Model.intersectional.QS.Gender <- brm(
+  formula = formula9,
+  data = dices,
+  family = cumulative("probit"),
+  prior = priors9,
+  warmup = 1000,
+  iter = 2000,
+  chains = 4,
+  seed = 42,
+  backend = 'rstan',
+  cores = 6
+)
+
+save(Model.intersectional.QS.Gender,file="ModelIntersectionalQSGender.RData")
+
+Model.intersectional.QSGE.Gender <- brm(
+  formula = formula10,
+  data = dices,
+  family = cumulative("probit"),
+  prior = priors10,
+  warmup = 1000,
+  iter = 2000,
+  chains = 4,
+  seed = 42,
+  backend = 'rstan',
+  threads = threading(8),
+  cores = 6
+)
+
+save(Model.intersectional.QSGE.Gender,file="ModelIntersectionalQSGEGender.RData")
 
 # Age
 formula11<- Q_overall ~ rater_age * (rater_raw_race +rater_gender +rater_education+phase) + (1 | rater_id) + (1 | item_id)
