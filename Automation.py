e180fd0c79591328ea5985e6e16f93bfdc83d511
ar1721 <rajbestaman1@gmail.com>
2023-11-08 08:41:21 -0500

    Running

--- Automation.py
+++ Automation.py
@@ -5,22 +5,38 @@ from os.path import isfile, join
 mypath="D:\RIT Study\Master Project\EVALUATION-OF-LLM-FOR-BIAS-IN-SAFETY-CONSIDERATION\With 990 and 350 Dataset\Code_Run"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-print(onlyfiles[0])
-for rfiles in onlyfiles:
-    filep="D:/RIT Study\Master Project\EVALUATION-OF-LLM-FOR-BIAS-IN-SAFETY-CONSIDERATION\With 990 and 350 Dataset\Code_Run/"+rfiles
-    f = open(filep,'r')
-    fw=open(rfiles,'w')
-    for line in f:
-        if line.__contains__("tidybayes"):
-            fw.write("#library(tidybayes)\n")
-        elif(line.__contains__("formula") and line.__contains__("rater_raw_race")):
-            line=line.replace("rater_raw_race","rater_ethinicity")
-            print(line)
-            fw.write(line)
-        else:
-            fw.write(line)
+# print(onlyfiles[0])
+# for rfiles in onlyfiles:
+#     filep="D:/RIT Study\Master Project\EVALUATION-OF-LLM-FOR-BIAS-IN-SAFETY-CONSIDERATION\With 990 and 350 Dataset\Code_Run/"+rfiles
+#     f = open(filep,'r')
+#     fw=open(rfiles,'w')
+#     for line in f:
+#         if line.__contains__("tidybayes"):
+#             fw.write("#library(tidybayes)\n")
+#         elif(line.__contains__("formula") and line.__contains__("rater_raw_race")):
+#             line=line.replace("rater_raw_race","rater_ethinicity")
+#             print(line)
+#             fw.write(line)
+#         else:
+#             fw.write(line)
             
 
-    # Close opend file
-    f.close()
+#     # Close opend file
+#     f.close()
+#     fw.close()
+
+for rsh in onlyfiles:
+    fsh=open("bm1.sh",'r')
+    sa=rsh.replace(".R","")
+    print(sa)
+    shw="bm"+sa+".sh"
+    fw=open("runningFiles/"+shw,'w')
+    for lines in fsh:
+        if lines.__contains__("R <"):
+            k="R < "+rsh+" --no-save"
+            fw.write(k)
+        else:
+            fw.write(lines)
+        
     fw.close()
+    fsh.close()
---
+++ bm1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#NOTE the -l flag!
+#
+# Name of the job 
+#SBATCH -J bayesian
+
+# Standard out and Standard Error output files
+#SBATCH -o %j_%x.output
+#SBATCH -e %j_%x.error
+
+#To send emails, set the adcdress below and remove one of the "#" signs.
+#SBATCH --mail-user=slack:@al3170
+
+# notify on state change: BEGIN, END, FAIL or ALL
+#SBATCH --mail-type=ALL
+
+# Request 5 hours run time MAX, anything over will be KILLED
+#SBATCH -t 3-15:0:0
+
+# Put the job in the "work" partition and request FOUR cores
+# "work" is the default partition so it can be omitted without issue.
+#SBATCH -p tier3 -n 5 -A safe-llm	
+
+# Job memory requirements in MB
+#SBATCH --mem=10g
+
+# Job gpu requirements
+##SBATCH --gres=gpu:1
+
+# Load R
+spack load r@4.2.2
+
+# Your job script goes below this line.  
+#
+R < ReplicationFamilyFullDatasetRawAD.R --no-save
+
